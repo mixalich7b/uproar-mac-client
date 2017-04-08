8008c1387f9d94678d33b37494547f720caf776e
k.tupitsin <mixalich7b@gmail.com>
2017-04-08 14:39:05 +0300

    Rename some folders

---
+++ uproar-mac-client/Service/TrackQueueManager.swift
@@ -0,0 +1,13 @@
+//
+//  TrackQueueManager.swift
+//  uproar-mac-client
+//
+//  Created by Тупицин Константин on 08.04.17.
+//  Copyright © 2017 mixalich7b. All rights reserved.
+//
+
+import Cocoa
+
+class TrackQueueManager: Any {
+
+}
